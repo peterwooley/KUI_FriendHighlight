388bd9e61a143704fd5f9796815091d73fff5ea0
Peter Wooley <peterwooley@gmail.com>
2022-05-10 23:06:38 -0700

    Initial version of KUI_FriendHighlight.

---
+++ KUI_FriendHighlight.lua
@@ -0,0 +1,74 @@
+local addon = KuiNameplates
+local mod = addon:NewPlugin('FriendHighlight',1005)
+
+local UnitIsPlayer,UnitIsOtherPlayersPet,GetGuildInfo=
+      UnitIsPlayer,UnitIsOtherPlayersPet,GetGuildInfo
+
+local function Frame_UpdateGuildText(f)
+    f.FriendHighlight_UpdateGuildText(f);
+
+    if UnitIsPlayer(f.unit) then
+        local friend = C_FriendList.IsFriend(UnitGUID(f.unit))
+        if friend then
+            mod:ShowFriendIcon(f)
+            return
+        end
+
+        local guild = f.state.guild_text
+        if not guild then return end
+
+        local myGuild = GetGuildInfo("player")
+        if not myGuild then return end
+
+        if guild == myGuild then
+            --print(f.state.name .. " is in your guild.")
+            mod:ShowGuildIcon(f)
+        else
+            --print(f.state.name .. " is not in your guild.")
+
+            
+        end
+    else
+        if f.FriendHighlight ~= nil then f.FriendHighlight:Hide() end
+    end
+end
+
+function mod:ShowGuildIcon(f)
+    local i = f:CreateTexture()
+    i:SetTexture(1981967)
+    i:SetTexCoord(0.2216796875, 0.2451171875, 0.947265625, 0.994140625);
+    i:SetHeight(17)
+    i:SetWidth(17)
+
+    mod:ShowIcon(f, i)
+end
+
+function mod:ShowFriendIcon(f)
+    local i = f:CreateTexture()
+    i:SetTexture("Interface/FriendsFrame/FriendsListFavorite")
+    i:SetTexCoord(0.03125, 0.5625, 0.03125, 0.5625);
+    i:SetHeight(17)
+    i:SetWidth(17)
+
+    mod:ShowIcon(f, i)
+end
+
+function mod:ShowIcon(f, i)
+    if f.FriendHighlight ~= nil then
+        f.FriendHighlight:Hide()
+        f.FriendHighlight = nil
+    end
+
+    i:SetPoint('LEFT',f.NameText,'RIGHT')
+    f.FriendHighlight = i
+end
+
+function mod:Create(f)
+    f.FriendHighlight_UpdateGuildText = f.UpdateGuildText
+    f.UpdateGuildText = Frame_UpdateGuildText
+end
+
+function mod:OnEnable()
+    self:RegisterMessage('Create')
+end
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# Kui Nameplates Friend Highlight
+
+Add an icon next to your friends and guildmates' nameplates with this plugin for [KuiNameplates](https://www.curseforge.com/wow/addons/kuinameplates).
+
+<img src="screenshots/friend.png" alt="Screenshot of Kui_FriendHighlight showing a star icon next to a friend's nameplate." />
+<img src="screenshots/guildmate.png" alt="Screenshot of Kui_FriendHighlight showing an orange diamond icon next to a guildmate's nameplate." />
+
+Note that [KuiNameplates](https://www.curseforge.com/wow/addons/kuinameplates) must be installed and running for this plugin to work.
+
+## Author
+Created by Peter Wooley
